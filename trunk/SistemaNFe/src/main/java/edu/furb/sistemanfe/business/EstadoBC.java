35058eb2a77af6d4d1946ceff6f5db840909069a
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-03-18 13:54:10 +0000

    Teste de commit.

--- trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/business/EstadoBC.java
+++ trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/business/EstadoBC.java
@@ -1,13 +1,10 @@
 package edu.furb.sistemanfe.business;
 
-import javax.inject.Inject;
-
 import br.gov.frameworkdemoiselle.lifecycle.Startup;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 import edu.furb.sistemanfe.domain.Estado;
-import edu.furb.sistemanfe.domain.Pais;
 import edu.furb.sistemanfe.persistence.EstadoDAO;
 
 @BusinessController
