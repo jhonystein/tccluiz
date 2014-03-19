2496419d05aaf81b37bea6de27efdb72d529e726
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-03-19 00:44:41 +0000

    Classes básicas de negocio e persistencia referente a Nota Fiscal.

---
+++ trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/business/NotaFiscalBC.java
@@ -0,0 +1,14 @@
+package edu.furb.sistemanfe.business;
+
+import br.gov.frameworkdemoiselle.stereotype.BusinessController;
+import br.gov.frameworkdemoiselle.template.DelegateCrud;
+import edu.furb.sistemanfe.domain.NotaFiscal;
+import edu.furb.sistemanfe.persistence.NotaFiscalDAO;
+
+@BusinessController
+public class NotaFiscalBC  extends DelegateCrud<NotaFiscal, Long, NotaFiscalDAO> {
+
+	private static final long serialVersionUID = -9033245211268617527L;
+
+
+}
