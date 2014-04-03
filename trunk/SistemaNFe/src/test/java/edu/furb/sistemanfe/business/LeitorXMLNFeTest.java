727ad02764ca463ec1447a1669a2642cd82e0d10
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-04-03 23:11:10 +0000

    Ajustes na impoirtação da nota; Adicionado campo Versao e data de importação na nota; Alterada a rotian de teste para criar um emitente a rotina de importacao só roda se tiver um emitente atribuido;;

--- trunk/SistemaNFe/src/test/java/edu/furb/sistemanfe/business/LeitorXMLNFeTest.java
+++ trunk/SistemaNFe/src/test/java/edu/furb/sistemanfe/business/LeitorXMLNFeTest.java
@@ -7,10 +7,15 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import br.gov.frameworkdemoiselle.junit.DemoiselleRunner;
+import edu.furb.sistemanfe.domain.Bookmark;
+import edu.furb.sistemanfe.domain.Emitente;
+import edu.furb.sistemanfe.domain.Endereco;
 import edu.furb.sistemanfe.domain.NotaFiscal;
 
 @RunWith(DemoiselleRunner.class)
@@ -18,12 +23,30 @@ public class LeitorXMLNFeTest {
 
 	@Inject
 	private LeitorXMLNFe nfeXmlReader;
+	@Inject
+	private EmitenteBC emitenteBC;
+	private Emitente emitenteTest;
+	
+	@Before
+	public void before() {
+		Endereco endEmit = new Endereco();
+		endEmit.setBairro("Bairro");
+		endEmit.setCep("CEP");
+		endEmit.setNumero("numero");
+		emitenteTest = new Emitente("000000000000", "EMITENTE PARA ROTINA DE TESTES", "ISENTO", endEmit);
+		emitenteBC.insert(emitenteTest);
+	}
+	
+	@After
+	public void after() {
+		emitenteBC.delete(emitenteTest.getId());
+	}
 
 	@Test
 	public void lerXmlSucesso() {
 
-		String caminho = "nfe.xml";
-
+		String caminho = "nfe-grande.xml";
+		nfeXmlReader.setEmitente(emitenteTest);
 		List<NotaFiscal> litaNotas = nfeXmlReader.readXml(caminho);
 		assertNotNull(litaNotas);
 		assertEquals(1, litaNotas.size());
