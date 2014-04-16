6719aea8caae2d1aa3a85611953007a67c77fd6c
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-04-16 00:09:26 +0000

    Ajustes para conexao com banco de dados na aplicação. Inicio da rotina de upload de arquivo.

---
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/UploadFileMB.java
@@ -0,0 +1,14 @@
+package edu.furb.sistemanfe.view;
+
+import br.gov.frameworkdemoiselle.annotation.PreviousView;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import br.gov.frameworkdemoiselle.template.AbstractPageBean;
+
+@ViewController
+@PreviousView("./upload_file.xhtml")
+public class UploadFileMB extends AbstractPageBean {
+
+	private static final long serialVersionUID = 8123642221316061192L;
+
+
+}
