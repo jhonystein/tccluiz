8bb109266a407a46a368e6b3af854311473bb24f
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-04-17 01:14:30 +0000

    Ajustes no layout e no Upload.

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/view/UploadFileMB.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/UploadFileMB.java
@@ -1,5 +1,11 @@
 package edu.furb.sistemanfe.view;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractPageBean;
@@ -10,5 +16,31 @@ public class UploadFileMB extends AbstractPageBean {
 
 	private static final long serialVersionUID = 8123642221316061192L;
 
+	//@Inject
+	//private MessageContext messageContext;
+
+	// public void handleFileUpload(FileUploadEvent event) {
+	// // FacesMessage msg = new FacesMessage("Succesful",
+	// event.getFile().getFileName() + " is uploaded.");
+	// // FacesContext.getCurrentInstance().addMessage(null, msg);
+	// //Message message = new
+	// DefaultMessage("Ocorreu um erro ao excluir o aluno!",
+	// SeverityType.ERROR);
+	// messageContext.add("Upload efetiado com sucesso!!", SeverityType.INFO);
+	//
+	//
+	// }
+
+	public void doUpload(FileUploadEvent fileUploadEvent) {
+		UploadedFile uploadedFile = fileUploadEvent.getFile();
+		String fileNameUploaded = uploadedFile.getFileName();
+		long fileSizeUploaded = uploadedFile.getSize(); // 3
+		String infoAboutFile = "<br/> Arquivo recebido: <b>" + fileNameUploaded
+				+ "</b><br/>" + "Tamanho do Arquivo: <b>" + fileSizeUploaded
+				+ "</b>";
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(null,
+				new FacesMessage("Sucesso", infoAboutFile));
+	}
 
 }
