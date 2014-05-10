079e5356b9189e539df34ded5d0de3ce5de18f5b
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-05-10 21:07:19 +0000

    Issue 7 Adicionados controles de acesso.

---
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/LoginMB.java
@@ -0,0 +1,68 @@
+package edu.furb.sistemanfe.view;
+
+import javax.inject.Inject;
+
+import edu.furb.sistemanfe.security.SistemaNFeCredentials;
+
+import br.gov.frameworkdemoiselle.annotation.NextView;
+import br.gov.frameworkdemoiselle.message.MessageContext;
+import br.gov.frameworkdemoiselle.message.SeverityType;
+import br.gov.frameworkdemoiselle.security.SecurityContext;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import br.gov.frameworkdemoiselle.template.AbstractPageBean;
+
+@ViewController
+@NextView("./index.jsf")
+//@NextView("./welcome.jsf")
+public class LoginMB extends AbstractPageBean {
+
+	private static final long serialVersionUID = 1L;
+
+	private String usuario = new String();
+	private String senha = new String();
+
+	@Inject
+	private SistemaNFeCredentials credentials;
+
+	@Inject
+	private SecurityContext securityContext;
+
+	@Inject
+	private MessageContext messageContext;
+
+	public String doLogin() {
+		try {
+			messageContext.add("TESTE!", SeverityType.INFO);
+			credentials.setUsername(this.getUsuario());
+			credentials.setPassword(this.getSenha());
+			securityContext.login();
+			messageContext.add("Bem vindo!", SeverityType.INFO);
+			return getNextView();
+		} catch (Exception e) {
+			messageContext.add(e.getMessage(), SeverityType.ERROR);
+			// AuthenticationException
+			return "";
+		}
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void doLogout() {
+		securityContext.logout();
+	}
+
+}
