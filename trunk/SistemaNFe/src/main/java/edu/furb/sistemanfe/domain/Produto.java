37b53cc9aed2b8766e07e0d45d52f6508780c120
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-05-24 17:35:15 +0000

    Pesquisa por produto

--- trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/domain/Produto.java
+++ trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/domain/Produto.java
@@ -28,6 +28,14 @@ public class Produto {
 	@JoinColumn(name = "IDEMITENTE")
 	private Emitente emitente;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public Emitente getEmitente() {
 		return emitente;
 	}
--- trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/persistence/ProdutoDAO.java
+++ trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/persistence/ProdutoDAO.java
@@ -1,6 +1,12 @@
 package edu.furb.sistemanfe.persistence;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
 import br.gov.frameworkdemoiselle.template.JPACrud;
@@ -13,7 +19,41 @@ public class ProdutoDAO extends JPACrud<Produto, Long> {
 	private static final long serialVersionUID = 1L;
 
 	public List<Produto> pesquisar(ProdutoDTO dto) {
-		return null;
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+		Root<Produto> objeto = query.from(Produto.class);
+		query.select(objeto);
+
+		List<Predicate> predicateList = new ArrayList<Predicate>();
+
+		if (dto.getId() != null) {
+			Predicate p = builder.equal(objeto.<Long> get("id"),
+					dto.getId());
+			predicateList.add(p);
+		}
+		if (dto.getCodigo() != null) {
+			Predicate p = builder.equal(objeto.<String> get("codigo"),
+					dto.getCodigo());
+			predicateList.add(p);
+		}
+		if (dto.getNome() != null) {
+			Predicate p = builder.equal(objeto.<String> get("nome"),
+					dto.getNome());
+			predicateList.add(p);
+		}
+		//TODO: mimplementar os demais campos;
+//		if (dto.getEmitente() == null) {
+			//TODO: Deve criticar se naum tem emitente;
+//			Predicate p = builder.equal(municipio.<String> get("nome"),
+//					municipioDTO.getNome());
+//			predicateList.add(p);
+//		}
+		
+		Predicate[] predicates = new Predicate[predicateList.size()];
+		predicateList.toArray(predicates);
+		query.where(predicates);	
+		return getEntityManager().createQuery(query).getResultList();
+
 	}
 	
 
