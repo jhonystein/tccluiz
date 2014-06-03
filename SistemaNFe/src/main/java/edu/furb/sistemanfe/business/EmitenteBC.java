cf5e8336a1fdfc0e4ded8044467048400d77da38
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-03 23:42:18 +0000

    Importar Emitente por XML;

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/business/EmitenteBC.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/business/EmitenteBC.java
@@ -1,17 +1,26 @@
 
 package edu.furb.sistemanfe.business;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 import edu.furb.sistemanfe.domain.Emitente;
+import edu.furb.sistemanfe.exception.ValidationException;
 import edu.furb.sistemanfe.persistence.EmitenteDAO;
 import edu.furb.sistemanfe.rest.EmitenteDTO;
 
 @BusinessController
 public class EmitenteBC extends DelegateCrud<Emitente, Long, EmitenteDAO> {
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private LeitorXMLEmitente leitorXMLEmitente;
 	
 	public Emitente buscaDocumento(String documento) {
 		EmitenteDTO dto = new EmitenteDTO();
@@ -21,5 +30,36 @@ public class EmitenteBC extends DelegateCrud<Emitente, Long, EmitenteDAO> {
 			return null;
 		}
 		return lst.get(0);	
+	}
+	
+	public boolean importarEmitenteXML(byte[] arquivo) throws ValidationException{
+		String destino = "";
+		File fileEmitente = null;
+		// Gera um arquivo temporário		
+		try {
+			fileEmitente = File.createTempFile(
+					String.format("%s-%s", "1", "EMITENTE"), ".XML");
+			destino = fileEmitente.getAbsolutePath();
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(
+					destino));
+			
+			fileOutputStream.write(arquivo);
+			fileOutputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ValidationException("Erro ao ler arquivo:"+e.getMessage());
+		}
+		try{
+			Emitente emit = leitorXMLEmitente.readXml(fileEmitente);
+			insert(emit);
+			
+		}catch (Exception e) {
+			throw new ValidationException("Falha ao ler arquivo XML."+e.getMessage());
+		}
+			
+			//return destino;
+
+		return false;
 	}
 }
---
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/exception/ValidationException.java
@@ -0,0 +1,12 @@
+package edu.furb.sistemanfe.exception;
+
+public class ValidationException extends Exception {
+	
+	public ValidationException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID = 3003845332149083978L;
+
+	
+}
