b639e3c21b4d29c947bc8674e325e843952e204f
luizmarian@gmail.com <luizmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-21 01:03:35 +0000

    serialVersionUID, é só apagar salvar a classe de depois clicar o icome amarelo Retirado usuário e configuração do top.xhtml Criado VendasCliente, VendasClienteMB, getClientesVendas na classe clienteBC Criado Metodo clienteVendas no clienteDAO Criado lista_vendas_cliente.xhtml Ajustado o flitro_vendas_cliente.xhtml

---
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasClienteMB.java
@@ -0,0 +1,56 @@
+package edu.furb.sistemanfe.view;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import edu.furb.sistemanfe.business.ClienteBC;
+import edu.furb.sistemanfe.pojo.ClienteVendas;
+
+import br.gov.frameworkdemoiselle.stereotype.ManagementController;
+
+@Named
+@ManagementController
+@RequestScoped
+public class VendasClienteMB implements Serializable {
+
+
+	private static final long serialVersionUID = -7051142694100439527L;
+
+	@Inject
+	private ClienteBC clienteBC;
+	
+	private Date dataIni = null;
+	private Date dataFim = null;
+	private List<ClienteVendas> listaDados = null;
+
+	public Date getDataIni() {
+		return dataIni;
+	}
+	public void setDataIni(Date dataIni) {
+		this.dataIni = dataIni;
+	}
+	public Date getDataFim() {
+		return dataFim;
+	}
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+	
+	public VendasClienteMB() {
+	}
+	
+	public String consultarRegistros() {
+		listaDados = clienteBC.getClientesVendas(this.dataIni, this.dataFim);
+		return "lista_vendas_cliente";
+	}
+	
+	public List<ClienteVendas> getListaDados() {
+		return listaDados;
+	}
+
+}
