d5c27742c1556fd9e88c69d4b7b9f4b318d45781
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-21 21:17:38 +0000

    Adicionado gráfico de vendas por região

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasRegiaoMB.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasRegiaoMB.java
@@ -1,12 +1,18 @@
 package edu.furb.sistemanfe.view;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 import br.gov.frameworkdemoiselle.stereotype.ManagementController;
 import edu.furb.sistemanfe.business.NotaFiscalBC;
@@ -21,6 +27,7 @@ public class VendasRegiaoMB implements Serializable {
 
 	@Inject
 	private NotaFiscalBC notaFiscalBC;
+	private BarChartModel graficoBarraVendas;
 	
 	private Date dataIni = null;
 	private Date dataFim = null;
@@ -49,6 +56,60 @@ public class VendasRegiaoMB implements Serializable {
 	
 	public List<RegiaoVendas> getListaDados() {
 		return listaDados;
+	}
+	
+	public String graficoVendas(){
+		listaDados = notaFiscalBC.getRegiaoVendas(this.dataIni, this.dataFim);
+		createBarModel();
+		return "grafico_vendas_regiao";
+	}
+	
+	public BarChartModel getGraficoBarraVendas() {
+		return graficoBarraVendas;
+	}
+	
+	private void createBarModel() {
+		BigDecimal maiorValor = new BigDecimal(0);
+		BigDecimal menorValor = new BigDecimal(0);
+		for (RegiaoVendas objeto : this.listaDados) {
+			if(objeto.getValor().compareTo(maiorValor)==1){
+				maiorValor = objeto.getValor();
+			}
+			if(objeto.getValor().compareTo(menorValor)==-1){
+				menorValor = objeto.getValor();
+			}
+		}
+		
+		maiorValor = maiorValor.add(maiorValor.multiply(new BigDecimal(0.10)));
+		
+		graficoBarraVendas = initBarModel();
+
+		graficoBarraVendas.setTitle("Vendas por município");
+		graficoBarraVendas.setLegendPosition("ne");
+
+		Axis xAxis = graficoBarraVendas.getAxis(AxisType.X);
+		xAxis.setLabel("Municípios");
+
+		Axis yAxis = graficoBarraVendas.getAxis(AxisType.Y);
+		yAxis.setLabel("Valores");
+		yAxis.setMin(menorValor);
+		yAxis.setMax(maiorValor);
+	}
+	
+	private BarChartModel initBarModel() {
+	
+		BarChartModel model = new BarChartModel();
+
+		ChartSeries boys = new ChartSeries();
+		boys.setLabel("Municípios");
+		
+		for (RegiaoVendas objeto : this.listaDados) {
+			boys.set(objeto.getMunicipio().getNome(), objeto.getValor());
+		}		
+
+		model.addSeries(boys);
+
+		return model;
 	}
 
 }
