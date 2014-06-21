949ceeb37c874d3515d83afe75fb6a7cb7b6dffc
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-21 23:46:51 +0000

    Ajustes para tentar evitar duplicidade de produtos e clientes;

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/business/NotaFiscalBC.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/business/NotaFiscalBC.java
@@ -10,12 +10,10 @@ import org.primefaces.model.SortOrder;
 
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
-import edu.furb.sistemanfe.domain.Cliente;
 import edu.furb.sistemanfe.domain.Emitente;
 import edu.furb.sistemanfe.domain.NotaFiscal;
 import edu.furb.sistemanfe.persistence.NotaFiscalDAO;
 import edu.furb.sistemanfe.pojo.RegiaoVendas;
-import edu.furb.sistemanfe.rest.ClienteDTO;
 import edu.furb.sistemanfe.rest.NotaFiscalDTO;
 import edu.furb.sistemanfe.security.SistemaNFeCredentials;
 
