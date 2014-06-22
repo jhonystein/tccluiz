8ddd1fd32f2e614eac3811e0075710283947322d
luizmarian@gmail.com <luizmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-22 01:44:15 +0000

    desabilitado os campos da nota fiscal edit Criado os graficos de produtos e clientes. removido do menu o item grafico de produto

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasClienteMB.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasClienteMB.java
@@ -1,12 +1,19 @@
 package edu.furb.sistemanfe.view;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.HorizontalBarChartModel;
 
 import edu.furb.sistemanfe.business.ClienteBC;
 import edu.furb.sistemanfe.pojo.ClienteVendas;
@@ -22,6 +29,7 @@ public class VendasClienteMB implements Serializable {
 
 	@Inject
 	private ClienteBC clienteBC;
+	private HorizontalBarChartModel graficoBarraVendas;
 	
 	private Date dataIni = null;
 	private Date dataFim = null;
@@ -50,6 +58,63 @@ public class VendasClienteMB implements Serializable {
 	
 	public List<ClienteVendas> getListaDados() {
 		return listaDados;
+	}
+	public String graficoVendas(){
+		listaDados = clienteBC.getClientesVendas(this.dataIni, this.dataFim);
+		createBarModel();
+		return "grafico_vendas_cliente";
+	}
+	
+	public BarChartModel getGraficoBarraVendas() {
+		return graficoBarraVendas;
+	}
+	
+	private void createBarModel() {
+		
+		BigDecimal maiorValor = new BigDecimal(0);
+		BigDecimal menorValor = new BigDecimal(0);
+		/*
+		 * Obtendos os dados de limite superior e inferior do gráfico;
+		 */
+		for (ClienteVendas objeto : this.listaDados) {
+			if(objeto.getValor().compareTo(maiorValor)==1){
+				maiorValor = objeto.getValor();
+			}
+			if(objeto.getValor().compareTo(menorValor)==-1){
+				menorValor = objeto.getValor();
+			}
+		}
+		
+		graficoBarraVendas = initBarModel();
+
+		graficoBarraVendas.setTitle("Vendas por clientes");
+		graficoBarraVendas.setLegendPosition("ne");
+
+		Axis xAxis = graficoBarraVendas.getAxis(AxisType.X);
+		xAxis.setLabel("Valores");
+
+		Axis yAxis = graficoBarraVendas.getAxis(AxisType.Y);
+		yAxis.setLabel("Clientes");
+		yAxis.setMin(menorValor);
+		yAxis.setMax(maiorValor);
+	}
+	
+	private HorizontalBarChartModel initBarModel() {
+	
+		HorizontalBarChartModel model = new HorizontalBarChartModel();
+
+		ChartSeries boys = new ChartSeries();
+		boys.setLabel("Clientes");
+		/**
+		 * Carregando os dados no gráfico
+		 */
+		for (ClienteVendas objeto : this.listaDados) {
+			boys.set(objeto.getCliente().getNome(), objeto.getValor());
+		}		
+
+		model.addSeries(boys);
+
+		return model;
 	}
 
 }
