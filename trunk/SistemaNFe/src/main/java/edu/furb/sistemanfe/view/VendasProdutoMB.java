82d544894d0ca19d2fb42b408409719f85d7f002
luizmarian@gmail.com <luizmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-22 01:44:15 +0000

    desabilitado os campos da nota fiscal edit Criado os graficos de produtos e clientes. removido do menu o item grafico de produto

--- trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasProdutoMB.java
+++ trunk/SistemaNFe/src/main/java/edu/furb/sistemanfe/view/VendasProdutoMB.java
@@ -1,6 +1,7 @@
 package edu.furb.sistemanfe.view;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -8,8 +9,15 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.HorizontalBarChartModel;
+
 import br.gov.frameworkdemoiselle.stereotype.ManagementController;
 import edu.furb.sistemanfe.business.ProdutoBC;
+import edu.furb.sistemanfe.pojo.ClienteVendas;
 import edu.furb.sistemanfe.pojo.ProdutoVendas;
 
 
@@ -22,6 +30,7 @@ public class VendasProdutoMB implements Serializable {
 
 	@Inject
 	private ProdutoBC produtoBC;
+	private HorizontalBarChartModel graficoBarraVendas;
 	
 	private Date dataIni = null;
 	private Date dataFim = null;
@@ -50,6 +59,63 @@ public class VendasProdutoMB implements Serializable {
 	
 	public List<ProdutoVendas> getListaDados() {
 		return listaDados;
+	}
+	public String graficoVendas(){
+		listaDados = produtoBC.getProdutosVendas(this.dataIni, this.dataFim);
+		createBarModel();
+		return "grafico_vendas_produto";
+	}
+	
+	public BarChartModel getGraficoBarraVendas() {
+		return graficoBarraVendas;
+	}
+	
+	private void createBarModel() {
+		
+		BigDecimal maiorValor = new BigDecimal(0);
+		BigDecimal menorValor = new BigDecimal(0);
+		/*
+		 * Obtendos os dados de limite superior e inferior do gráfico;
+		 */
+		for (ProdutoVendas objeto : this.listaDados) {
+			if(objeto.getValor().compareTo(maiorValor)==1){
+				maiorValor = objeto.getValor();
+			}
+			if(objeto.getValor().compareTo(menorValor)==-1){
+				menorValor = objeto.getValor();
+			}
+		}
+		
+		graficoBarraVendas = initBarModel();
+
+		graficoBarraVendas.setTitle("Vendas por produtos");
+		graficoBarraVendas.setLegendPosition("ne");
+
+		Axis xAxis = graficoBarraVendas.getAxis(AxisType.X);
+		xAxis.setLabel("Valores");
+
+		Axis yAxis = graficoBarraVendas.getAxis(AxisType.Y);
+		yAxis.setLabel("Produtos");
+		yAxis.setMin(menorValor);
+		yAxis.setMax(maiorValor);
+	}
+	
+	private HorizontalBarChartModel initBarModel() {
+	
+		HorizontalBarChartModel model = new HorizontalBarChartModel();
+
+		ChartSeries boys = new ChartSeries();
+		boys.setLabel("Produtos");
+		/**
+		 * Carregando os dados no gráfico
+		 */
+		for (ProdutoVendas objeto : this.listaDados) {
+			boys.set(objeto.getProduto().getNome(), objeto.getValor());
+		}		
+
+		model.addSeries(boys);
+
+		return model;
 	}
 
 }
