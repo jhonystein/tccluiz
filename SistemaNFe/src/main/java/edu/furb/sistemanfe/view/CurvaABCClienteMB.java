0ead715041d3ef10bfa065d68c3e09fd0717851d
carlosmarian@gmail.com <carlosmarian@gmail.com@ed3b90fe-6584-71e0-4527-f03d1504a98b>
2014-06-23 20:40:14 +0000

    Grafico de pizza com os dados de Curva ABC de Cliente;

--- SistemaNFe/src/main/java/edu/furb/sistemanfe/view/CurvaABCClienteMB.java
+++ SistemaNFe/src/main/java/edu/furb/sistemanfe/view/CurvaABCClienteMB.java
@@ -1,13 +1,20 @@
 package edu.furb.sistemanfe.view;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.chart.PieChartModel;
+
+import br.gov.frameworkdemoiselle.message.MessageContext;
+import br.gov.frameworkdemoiselle.message.SeverityType;
 import br.gov.frameworkdemoiselle.stereotype.ManagementController;
 import edu.furb.sistemanfe.business.ClienteBC;
 import edu.furb.sistemanfe.pojo.ClienteCurvaABC;
@@ -21,10 +28,13 @@ public class CurvaABCClienteMB implements Serializable {
 
 	@Inject
 	private ClienteBC clienteBC;
+	@Inject
+	private MessageContext messageContext;
 	
 	private Date dataIni = null;
 	private Date dataFim = null;
 	private List<ClienteCurvaABC> listaDados = null;
+	private PieChartModel pieModel1;
 
 	public Date getDataIni() {
 		return dataIni;
@@ -49,5 +59,46 @@ public class CurvaABCClienteMB implements Serializable {
 	
 	public List<ClienteCurvaABC> getDadosCurvaABC() {
 		return this.listaDados;
+	}
+	
+	public String graficoCurvaABC(){
+		try{
+			listaDados = clienteBC.getDadosCurvaABC(this.dataIni, this.dataFim);
+			createPieModel();
+			return "grafico_curva_abc_cliente";
+		}catch(Exception ex){
+			messageContext
+			.add("Falha ao tentar obter dados para o gráfico: {0}", SeverityType.ERROR, ex.getMessage());
+			return "";
+		}
+	}
+	
+	private void createPieModel() {
+        pieModel1 = new PieChartModel();
+        BigDecimal total = new BigDecimal(0);
+        
+        Map<String, BigDecimal> listDados = new HashMap<String, BigDecimal>();
+        
+        for (ClienteCurvaABC object : listaDados) {
+        	if(listDados.containsKey(object.getClassificacao())){
+        		listDados.put(object.getClassificacao(), listDados.get(object.getClassificacao()).add(object.getValorTotal()) );        		
+        	}else{
+        		listDados.put(object.getClassificacao(), object.getValorTotal());
+        	}
+        	total = total.add(object.getValorTotal());
+		}
+        
+        java.text.DecimalFormat df = new java.text.DecimalFormat( "R$ #,##0.00" );
+        for (String key : listDados.keySet()) {
+        	pieModel1.set(key + " "+ df.format( listDados.get(key) ), listDados.get(key));
+		}
+        
+        pieModel1.setTitle("Por valor: "+ df.format( total )  );
+        pieModel1.setShowDataLabels(true);
+        pieModel1.setLegendPosition("w");
+    }
+	
+	public PieChartModel getGraficoCurvaABC() {
+		return pieModel1;
 	}
 }
